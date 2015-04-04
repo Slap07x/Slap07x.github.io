b6f7ab2001dc93a760dedeb3e9bd4f72b1acf2e0
Mike Howard <Slap07x@gmail.com>
2015-04-04 07:25:14 -0400

    Added index.html

---
+++ js/script.js
@@ -0,0 +1,115 @@
+$(function() {
+	$("#mercnav").click(function() {
+		$('html, body').animate({
+    		scrollTop: $("#mercury").offset().top
+		}, 1500);
+	});
+
+	$("#venusnav").click(function() {
+		$('html, body').animate({
+    		scrollTop: $("#venus").offset().top
+		}, 1500);
+	});
+
+	$("#earthnav").click(function() {
+		$('html, body').animate({
+    		scrollTop: $("#earth").offset().top
+		}, 1500);
+	});
+
+	$("#marsnav").click(function() {
+		$('html, body').animate({
+    		scrollTop: $("#mars").offset().top
+		}, 1500);
+	});
+
+	$("#jupnav").click(function() {
+		$('html, body').animate({
+    		scrollTop: $("#jupiter").offset().top
+		}, 1500);
+	});
+
+	$("#satnav").click(function() {
+		$('html, body').animate({
+    		scrollTop: $("#saturn").offset().top
+		}, 1500);
+	});
+
+	$("#uranav").click(function() {
+		$('html, body').animate({
+    		scrollTop: $("#uranus").offset().top
+		}, 1500);
+	});
+
+	$("#nepnav").click(function() {
+		$('html, body').animate({
+    		scrollTop: $("#neptune").offset().top
+		}, 1500);
+	});
+
+	$("#plutonav").click(function() {
+		$('html, body').animate({
+    		scrollTop: $("#pluto").offset().top
+		}, 1500);
+	});
+
+	$("#backtotop").click(function() {
+		$('html, body').animate({
+    		scrollTop: $("#123").offset().top
+		}, 1500);
+	});
+
+    $("#merctop").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#123").offset().top
+        }, 1500);
+    });
+
+    $("#ventop").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#123").offset().top
+        }, 1500);
+    });
+
+    $("#earthtop").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#123").offset().top
+        }, 1500);
+    });
+
+    $("#marstop").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#123").offset().top
+        }, 1500);
+    });
+
+    $("#juptop").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#123").offset().top
+        }, 1500);
+    });
+
+    $("#sattop").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#123").offset().top
+        }, 1500);
+    });
+
+    $("#uratop").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#123").offset().top
+        }, 1500);
+    });
+
+	$("#neptop").click(function() {
+		$('html, body').animate({
+    		scrollTop: $("#123").offset().top
+		}, 1500);
+	});
+
+    $("#plutotop").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#123").offset().top
+        }, 1500);
+    });
+});
